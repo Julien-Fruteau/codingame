60a88876d186a02b1018909f7e028d22cc889bef
julien <julien.fruteau@gmail.com>
2020-11-27 11:10:18 +0100

    the_lost_files: refact __add__ - use same behaviour as __iadd__

--- src/puzzle_of_the_week/the_lost_files/classes.py
+++ src/puzzle_of_the_week/the_lost_files/classes.py
@@ -30,6 +30,24 @@ class Edge(list):
         return self.vertices[s]
 
 
+class Tile:
+    """Composed of a set of Vertices and a set Edges
+    """
+
+    def __init__(self):
+        self.vertices: set[Vertex]
+        self.edges: set[Edge]
+
+    def add(self, v: Vertex) -> None:
+        pass
+        # self.vertices.append(v)
+
+
+class Continent:
+    def __init__(self, tile: Tile=None) -> None:
+        self.tile: list[Tile] = [tile] if tile else []
+
+
 class Graph:
     def __init__(self, edges: Iterable[Edge]=None):
         self.E: list[Edge] = list(edges) if edges else []
@@ -42,7 +60,8 @@ class Graph:
         return sorted(set(res))
 
     def __add__(self, other: Edge):
-        self.E.append(other)
+        if other not in self.E:
+            self.E.append(other)
         return Graph(self.E)
 
     def __iadd__(self, other: Edge):
@@ -57,48 +76,3 @@ class Graph:
 
 #     def add(self, edge: Edge) -> None:
 #         self.edges.append(edge)
-
-
-class Tile:
-    """Composed of a set of Vertices and a set Edges
-    """
-    def __init__(self):
-        self.vertices: set[Vertex]
-        self.edges: set[Edge]
-
-    def add(self, v: Vertex) -> None:
-        pass
-        # self.vertices.append(v)
-
-
-class TileManager:
-    def __init__(self) -> None:
-        self.tiles: list[Tile] = []
-    # if total tile = 0
-    #  create a new tile
-    #  add the edge
-    # else
-    #  for vertex in edge
-    #   if vertex
-
-
-class Continent:
-    def __init__(self, tile: Tile) -> None:
-        self.tile: list[Tile] = []
-        self.tile.append(tile)
-
-
-class ContinentManager:
-    pass
-    # while Tile
-        # if len(continent) = 0
-        #   create Continent(current_tile)
-        # else
-        # for continent in continents
-        #   if a tile as an edge in common with all current continent tile
-        #     add tile to continent
-        #   else
-        #     continue
-        # if tile not added
-        #   create Continent(current_tile)
-
